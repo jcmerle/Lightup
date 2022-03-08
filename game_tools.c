b8e974375f869205d22e02213f286c1c6475f24f
Jean-Charles Merle <jean-charles.merle@etu.u-bordeaux.fr>
2022-03-08 22:08:33 +0100

    Update game_tools.c

--- game_tools.c
+++ game_tools.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "assert.h"
 #include "game.h"
 #include "game_aux.h"
 #include "game_ext.h"
 #include "game_private.h"
 #include "game_test.h"
-#include "assert.h"
 
 game game_load(char* filename)
 {
@@ -49,9 +49,8 @@ game game_load(char* filename)
         fprintf(stderr, "Wrong state\n");
         exit(EXIT_FAILURE);
       }
-    } 
-    //fscanf(file, "%c", &c); //We ignore the /n
-
+    }
+    // fscanf(file, "%c", &c); //We ignore the /n
   }
   fclose(file);
   game_update_flags(g);
@@ -92,98 +91,115 @@ void game_save(cgame g, char* filename)
   fclose(file);
 }
 
-bool game_solve_aux(int nb_rows,int nb_cols,int coord_i, int coord_j, game g){
-
-  //Recurrence stop condition
-  if(coord_i==nb_rows && coord_j==0){
+bool game_solve_aux(int nb_rows, int nb_cols, int coord_i, int coord_j, game g)
+{
+  // Recurrence stop condition
+  if (coord_i == nb_rows && coord_j == 0) {
     return game_is_over(g);
   }
 
- //Double loop to see if the game has an error and so if it is necessary to continue
- //if the game has an error we stop and return false.
- //It is a really effective optimisation
- for(int y = 0; y < nb_rows; y++)
-  {
-    for(int x = 0; x < nb_cols; x++)
-    {
-      if (game_has_error(g, y, x))
-      {
+  // Double loop to see if the game has an error and so if it is necessary to continue
+  // if the game has an error we stop and return false.
+  // It is a really effective optimisation
+  for (int y = 0; y < nb_rows; y++) {
+    for (int x = 0; x < nb_cols; x++) {
+      if (game_has_error(g, y, x)) {
         return false;
       }
     }
   }
 
-  //We put a lightbulb in the squares which are not lighted (optimisation of the algorithm)
-  if(game_check_move(g, coord_i, coord_j, S_LIGHTBULB) && !game_is_lighted(g, coord_i, coord_j))
-  {
+  // We put a lightbulb in the squares which are not lighted (optimisation of the algorithm)
+  if (game_check_move(g, coord_i, coord_j, S_LIGHTBULB) && !game_is_lighted(g, coord_i, coord_j)) {
     game_play_move(g, coord_i, coord_j, S_LIGHTBULB);
   }
-  
-  //we call the function on the next square which depends of the current position 
-  if(coord_j==nb_cols-1)
-  {
-    if(game_solve_aux(nb_rows,nb_cols,coord_i + 1, 0, g))
-    {
+
+  // we call the function on the next square which depends of the current position
+  if (coord_j == nb_cols - 1) {
+    if (game_solve_aux(nb_rows, nb_cols, coord_i + 1, 0, g)) {
       return true;
     }
-  }
-  else
-  {
-    if(game_solve_aux(nb_rows,nb_cols,coord_i, coord_j+1, g))
-    {
+  } else {
+    if (game_solve_aux(nb_rows, nb_cols, coord_i, coord_j + 1, g)) {
       return true;
     }
   }
 
-  //And we do the same but not with a lightbulb but with a blank state.
-  //It is necessary to go through all the different possible combinations.
+  // And we do the same but not with a lightbulb but with a blank state.
+  // It is necessary to go through all the different possible combinations.
 
-  if(game_check_move(g, coord_i, coord_j, S_BLANK))
-  {
-    game_play_move(g,coord_i,coord_j,S_BLANK);
+  if (game_check_move(g, coord_i, coord_j, S_BLANK)) {
+    game_play_move(g, coord_i, coord_j, S_BLANK);
   }
 
-  if(coord_j==nb_cols-1)
-  {
-    if(game_solve_aux(nb_rows,nb_cols,coord_i + 1, 0, g))
-    {
+  if (coord_j == nb_cols - 1) {
+    if (game_solve_aux(nb_rows, nb_cols, coord_i + 1, 0, g)) {
       return true;
     }
-  }
-  else
-  {
-    if(game_solve_aux(nb_rows,nb_cols,coord_i, coord_j+1, g))
-    {
+  } else {
+    if (game_solve_aux(nb_rows, nb_cols, coord_i, coord_j + 1, g)) {
       return true;
     }
   }
 
-  //If we don't find a solution then we return false 
-  //and we go back to the previous square
+  // If we don't find a solution then we return false
+  // and we go back to the previous square
 
   return false;
 }
-
 
 bool game_solve(game g)
 {
   assert(g);
   game copy = game_copy(g);
 
-  if(game_solve_aux(game_nb_rows(g), game_nb_cols(g), 0, 0, g))
-  {
+  if (game_solve_aux(game_nb_rows(g), game_nb_cols(g), 0, 0, g)) {
     return true;
   }
-  
-  //On renvoie g à l'état initial si on a pas trouvé
-  else
-  {
+
+  // On renvoie g à l'état initial si on a pas trouvé
+  else {
     g = game_copy(copy);
     return false;
   }
 }
 
+uint game_nb_solutions_aux(uint coord_i, uint coord_j, uint* nb_sol, game g)
+{
+  if (coord_i == game_nb_rows(g) && coord_j==0) {
+    if (game_is_over(g)) {
+      (*nb_sol)++;
+    }
+    return (*nb_sol);
+  }
+
+  if (game_check_move(g, coord_i, coord_j, S_LIGHTBULB) && !game_is_lighted(g, coord_i, coord_j)) {
+    game_play_move(g, coord_i, coord_j, S_LIGHTBULB);
+  }
+
+  if (coord_j == game_nb_cols(g)-1) {
+    return game_nb_solutions_aux(coord_i + 1, 0, nb_sol, g);
+  }else{
+    return game_nb_solutions_aux(coord_i, coord_j + 1, nb_sol, g);
+  }
+
+  if (game_check_move(g, coord_i, coord_j, S_BLANK)) {
+    game_play_move(g, coord_i, coord_j, S_BLANK);
+  }
+
+  if(coord_j==game_nb_cols(g)-1){
+    return game_nb_solutions_aux(coord_i + 1, 0, nb_sol, g);
+  }else{
+    return game_nb_solutions_aux(coord_i, coord_j + 1, nb_sol, g);
+  }
+
+}
+
 uint game_nb_solutions(cgame g)
 {
-  return 0;
+  assert(g);
+  game copy = game_copy(g);
+  uint nb_sol = 0;
+
+  return game_nb_solutions_aux(0, 0, &nb_sol, copy);
 }
